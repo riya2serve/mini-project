739eff171cf511c40229f3c9e680f62af73b1864
Riya Rampalli <riyarampalli@Riyas-MacBook-Pro-7.local>
2025-03-05 12:01:46 -0500

    created new Python script for populating my genomes

---
+++ mini-project/__populatenome__.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "35b99732-bf46-4965-be99-e600d0e9ac42",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}

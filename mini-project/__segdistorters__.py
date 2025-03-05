0ae9f423570275b47b290de1e4166231dddb36d7
Riya Rampalli <riyarampalli@Riyas-MacBook-Pro-7.local>
2025-03-05 12:12:49 -0500

    created new Python script for manipulating segregation distortion

---
+++ mini-project/__segdistorters__.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "15d1df48-74bd-44e2-bff9-85ab48d5e7fe",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}

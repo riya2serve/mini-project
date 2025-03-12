986e2ddf6a6ad2ae94d2c1d88d51b930d4ef9e86
Riya Rampalli <riyarampalli@MacBookPro.lan>
2025-03-12 07:41:26 -0400

    making changes

--- mini-project/__populatenome__.py
+++ mini-project/__populatenome__.py
@@ -1,35 +1,42 @@
 from Bio import Entrez, SeqIO
+import os 
+import random 
+import pandas as pd 
 
-#provide an email bc NCBI requires it
+#================
+#CONFIGURATION
+#================
+#provide an email bc NCBI requires it?
 Entrez.email = "rr3491@columbia.edu"
 
+#================
+#MY FUNCTIONS
+#================
+
+#Step 1. make an output directory
+os.makedirs(output_folder, exist_ok = True)
+
+output_FASTA = f"{output_folder}{accession_id}.fasta"
+
+
 #let's create a function to fetch genome sequences from NCBI
-def fetch_nomes(accession_ids, output_folder = ""):
+def fetch_nomes(accession_id, output_folder = "spinach_genome/"):
     """
-    This function will fetch genome sequences from NCBI using the accession numbers provided by the user.
-    Once located, the acessions will be saved as a FASTA file.
+    This function will fetch genomesfrom NCBI using accession ids provided by the user. 
+    Once located, the genome(s) will be saved as a FASTA file.
 
-    accession_ids (parameter): user to provide NCBI accession number
-    output_folder (parameter): folder to save the FASTA files (default: current user director)
+    Parameters:
+    accession_id (string - str): NCBI accession number
+    output_folder (str): folder to save the FASTA file(s) (default: current user director)
     """
 
-    for accession_id in accession_ids:
-        output_fasta = f"{output_folder}{accession_id}.fasta"
 
-        try:
-            #fetch sequence(s) in FASTA format
-            genome = Entrez.efetch(db = "nucleotide", id = accession_id, rettype = "fasta", retmode = "text")
 
-            #save to output file
-            with open(output_fasta, "w") as file:
-                file.write(handle.read())
 
-            genome.close()
-            print(f"Genome {accession_id} saved as {output_fasta}")
 
-        except Exception as e:
-            print(f"Error in fetching genome {accession_id}: {e}")
 
-fetch_nomes(["GCA_900095335.1", ""]) 
-#first genome is an S. latifolia (male)
+
+
+
+
 #second genome is an 
